406710a14d1fc48af2f2561be27e598fb670b6ed
Mikael Mortensen <mikaem@math.uio.no>
2023-08-31 09:36:36 +0000

    Update python 3

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
-print "Hello world! from rank", comm.Get_rank(), "of", comm.Get_size(), "processors"
+print("Hello world! from rank", comm.Get_rank(), "of", comm.Get_size(), "processors")
